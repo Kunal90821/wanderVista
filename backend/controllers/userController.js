126fbeb8dc70eb69cbb99bf5b3ebdc688bea7f4e
kunal90821 <chaudharykunal581@ggmail.com>
2023-12-27 14:20:32 +0530

    Add Login and Logout function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,3 +31,48 @@ export const register = async(req,res,next) => {
         }
     });
 };
+
+
+// Login User
+
+export const login = async(req,res,next) => {
+    const {username, password} = req.body;
+
+    const user = new User({
+        username,
+        password
+    });
+
+    req.login(user,(err) => {
+        if(err) {
+            return res.status(500).json({
+                err
+            });
+        } else {
+            passport.authenticate("local")(req,res,()=> {
+                return res.status(201).json({
+                    success: true,
+                    user
+                });
+            });
+        }
+    });
+};
+
+
+// Logout User
+
+export const logOut = async(req,res,next) => {
+    req.logout(err => {
+        if(err) {
+            return res.status(500).json({
+                err
+            });
+        }
+        res.status(201).json({
+            success: true,
+            message: 'User logged out successfully'
+        });
+    });
+};
+
