5a868b2f87dcf3d1ebf862438153944fad864dd2
kunal90821 <chaudharykunal581@ggmail.com>
2024-02-09 21:02:06 +0530

    Added Footer, Popular category and optimized BlogsSlider

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -3,12 +3,16 @@ import Banner from './Banner'
 import BlogsSlider from './BlogsSlider'
 
 import '../styles/Animation.css'
+import PopularCategory from './PopularCategory'
+import Footer from './Footer'
 
 const Home = () => {
     return (
         <>
             <Banner className='animate-flip-up' />
             <BlogsSlider className='animate-flip-up' />
+            <PopularCategory className='animate-flip-up' />
+            <Footer className='animate-fade-in-up'/>
         </>
     )
 }
---
+++ frontend/src/components/PopularCategory.jsx
@@ -0,0 +1,44 @@
+import '../styles/PopularCategory.css';
+import '../styles/Animation.css';
+
+const PopularCategory = () => {
+    return (
+        <div className="bg-white main text-center lg:ml-6 ml-2 mt-4">
+            <div className='pt-8 pb-6 text-5xl font-bold animate-fade-up-delay'>Popular Category</div>
+            <section className="hero-section">
+                <div className="card-grid">
+                    <a className="card" href="#">
+                        <div className="card__background" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1557177324-56c542165309?ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80)'}}></div>
+                        <div className="card__content">
+                            <p className="card__category">Category</p>
+                            <h3 className="card__heading">Example Card Heading</h3>
+                        </div>
+                    </a>
+                    <a className="card" href="#">
+                        <div className="card__background" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1557187666-4fd70cf76254?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)'}}></div>
+                        <div className="card__content">
+                            <p className="card__category">Category</p>
+                            <h3 className="card__heading">Example Card Heading</h3>
+                        </div>
+                    </a>
+                    <a className="card" href="#">
+                        <div className="card__background" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1556680262-9990363a3e6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)'}}></div>
+                        <div className="card__content">
+                            <p className="card__category">Category</p>
+                            <h3 className="card__heading">Example Card Heading</h3>
+                        </div> 
+                    </a>
+                    <a className="card" href="#">
+                        <div className="card__background" style={{backgroundImage: 'url(https://images.unsplash.com/photo-1557004396-66e4174d7bf6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)'}}></div>
+                        <div className="card__content">
+                            <p className="card__category">Category</p>
+                            <h3 className="card__heading">Example Card Heading</h3>
+                        </div>
+                    </a>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default PopularCategory
